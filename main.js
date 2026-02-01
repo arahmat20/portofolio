42a90e32d4ea92432091e90a983bb2cd39676284
User <agungrahmat662@gmail.com>
2026-02-01 15:19:34 +0700

    add contact validasi

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 function toggleDetail(event) {
-    const button = event.target;
-    const expItem = button.closest(".about-exp-item");
-    const detail = expItem.querySelector(".about-exp-item-detail");
-  
-    if (detail.style.display === "block") {
-      detail.style.display = "none";
-      button.textContent = "More Info";
-      button.classList.remove("active");
-    } else {
-      detail.style.display = "block";
-      button.textContent = "Less Info";
-      button.classList.add("active");
-    }
+  const button = event.target;
+  const expItem = button.closest(".about-exp-item");
+  const detail = expItem.querySelector(".about-exp-item-detail");
+
+  if (detail.style.display === "block") {
+    detail.style.display = "none";
+    button.textContent = "More Info";
+    button.classList.remove("active");
+  } else {
+    detail.style.display = "block";
+    button.textContent = "Less Info";
+    button.classList.add("active");
   }
-  
+}
+function onformsubmit(e) {
+  e.preventDefault();
+  const email = $("#inp_email");
+  const subject = $("#inp_subject");
+  const massage = $("#inp_message");
+
+  if (!$(email).val()) {
+    alert("email is required");
+  } else if (!$(subject).val()) {
+    alert("subject is required");
+  } else if (!$(massage).val()) {
+    alert("message is required");
+  } else {
+    alert("form submitted");
+    $(email).val("");
+    $(subject).val("");
+    $(message).val("");
+  }
+}
